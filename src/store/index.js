dda8e85a8dff917453a0dde4db4e47b2893d6919
Marta G <marta.guszek@gmail.com>
2020-07-28 17:22:28 +0200

    add hardcoded state

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,39 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    restaurants: [
+      { id:1,
+        name: "jfgd",
+        rating:5,
+        cuisine:"french"
+     },
+     { id:2,
+       name: "lorem",
+       rating:4.9,
+       cuisine:"japanese"
+    },
+    { id:3,
+      name: "tudum",
+      rating:4.7,
+      cuisine:"polish"
+   },
+   { id:4,
+     name: "badum",
+     rating:4.5,
+     cuisine:"french"
+  },
+  { id:5,
+    name: "dunama",
+    rating:5,
+    cuisine:"french"
+ }
+   ]
+  },
   mutations: {},
   actions: {},
+  getters: {
+    allRestaurants: state => state.restaurants
+  },
   modules: {}
 });
