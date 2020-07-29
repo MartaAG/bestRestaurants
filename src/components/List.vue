21f8742cea660d3724f37a6816460642b1479a9b
Marta G <marta.guszek@gmail.com>
2020-07-29 12:48:21 +0200

    add basic vuetify components

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,25 +1,59 @@
 <template>
-  <div>
+<div>
 
 
   <h1>best restaurants from Wroclaw, dzień dobry!</h1>
-    <div v-for="restaurant in allRestaurants" :key="restaurant.id">
-        <h2>{{restaurant.name}}</h2>
-        <p>
-          {{restaurant.rating}}
-        </p>
-        <p>
-          {{restaurant.cuisine}}
-        </p>
-    </div>
 
-    </div>
+  <v-card class="mx-auto my-12" max-width="374" v-for="restaurant in allRestaurants" :key="restaurant.id">
+
+    <v-img height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
+
+
+    <v-card-title>{{restaurant.name}}
+    </v-card-title>
+    <v-card-text>
+      <v-row align="center" class="mx-0">
+        <v-rating
+        :value="restaurant.rating"
+        empty-icon="mdi-heart-outline"
+        full-icon="mdi-heart"
+        half-icon="mdi-heart-half-full" color="red"
+        background-color="grey lighten-2"
+        half-increments
+        readonly
+        size="30">
+        </v-rating>
+        <div class="grey--text ml-5">
+          {{restaurant.rating}}
+        </div>
+
+        <div  class="my-4 subtitle-1">
+          Cuisine: {{restaurant.cuisine}}
+        </div>
+      </v-row>
+
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        color="deep-purple lighten-2"
+         text
+         >
+        Details
+      </v-btn>
+    </v-card-actions>
+
+
+  </v-card>
+
+</div>
 </template>
 
 <script>
-  import {mapGetters} from 'vuex'
-  export default {
-    name: 'List',
-    computed: mapGetters(['allRestaurants'])
-  }
+import {
+  mapGetters
+} from 'vuex'
+export default {
+  name: 'List',
+  computed: mapGetters(['allRestaurants'])
+}
 </script>
