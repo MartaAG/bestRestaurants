de088db72a039f70df63bad4676e5c3ce49bb4a4
Marta G <marta.guszek@gmail.com>
2020-07-29 14:57:24 +0200

    fix formating

--- src/components/List.vue
+++ src/components/List.vue
@@ -2,18 +2,34 @@
 <div>
 
   <h1>best restaurants from Wroclaw, dzień dobry!</h1>
-  <v-row align="center" justify="center" class="grey lighten-5">
+  <v-row
+  align="center"
+  justify="center"
+  class="grey lighten-5">
 
-    <v-card class="mx-auto my-12" max-width="374" v-for="restaurant in allRestaurants" :key="restaurant.id">
+    <v-card
+    class="mx-auto my-12"
+    max-width="374"
+    v-for="restaurant in allRestaurants"
+    :key="restaurant.id">
 
-      <v-img height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
+      <v-img
+      height="250"
+      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
 
 
       <v-card-title>{{restaurant.name}}
       </v-card-title>
       <v-card-text>
         <v-row align="center" class="mx-0">
-          <v-rating :value="restaurant.rating" empty-icon="mdi-heart-outline" full-icon="mdi-heart" half-icon="mdi-heart-half-full" color="red" background-color="grey lighten-2" half-increments readonly size="30">
+          <v-rating
+          :value="restaurant.rating"
+          empty-icon="mdi-heart-outline"
+          full-icon="mdi-heart"
+          half-icon="mdi-heart-half-full"
+          color="red"
+          background-color="grey lighten-2"
+          half-increments readonly size="30">
           </v-rating>
           <div class="grey--text ml-5">
             {{restaurant.rating}}
@@ -48,13 +64,3 @@ export default {
   computed: mapGetters(['allRestaurants'])
 }
 </script>
-
-<style scoped>
-.row {
-  border: 1px solid red;
-}
-
-.col {
-  border: 1px solid blue;
-}
-</style>
