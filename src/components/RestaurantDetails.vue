64b3b8109120dd1cb2a9bc0ae8f5c42a70239349
Marta G <marta.guszek@gmail.com>
2020-07-30 16:17:22 +0200

    add details page

---
+++ src/components/RestaurantDetails.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h2>Name of the restaurant</h2>
+    <h3>place</h3>
+    <p>
+      adres
+    </p>
+    <p>
+      rodzaj kuchni
+    </p>
+    <p>
+      średni koszt obiadu
+    </p>
+    <p>
+      ocena
+    </p>
+    <p>
+      czy obsługuje zamówienia przez internet?
+    </p>
+    <p>
+      czy ma rezerwację miejsc?
+    </p>
+    <div>
+      jakieś linki do zdjęć, menu i wydarzeń
+    </div>
+
+  <v-btn @click="goBack">
+    go back
+  </v-btn>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "RestaurantDetails",
+  methods: {
+    goBack() {
+      return this.$router.go(-1)
+    }
+  }
+}
+
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-  </div>
+    <RestaurantDetails />
+    </div>
 </template>
+
+<script>
+
+import RestaurantDetails from "@/components/RestaurantDetails";
+
+export default {
+  name: 'About',
+  components: {
+    RestaurantDetails
+  }
+}
+
+
+</script>
