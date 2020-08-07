d4595a616ca59557bdfc234aaf2c08d5867d94d8
Marta G <marta.guszek@gmail.com>
2020-08-07 16:22:48 +0200

    add interceptors

--- src/main.js
+++ src/main.js
@@ -11,6 +11,37 @@ Vue.use(VueAxios, axios);
 axios.defaults.baseURL = "https://developers.zomato.com/api/v2.1";
 axios.defaults.headers.common["user-key"] = process.env.VUE_APP_API_KEY;
 
+//interceptors
+axios.interceptors.request.use(
+  config => {
+    if (process.env.VUE_APP_API_KEY) {
+      console.info("✉️ ", config);
+    }
+    return config;
+  },
+  error => {
+    if (process.env.VUE_APP_API_KEY) {
+      console.error("✉️ ", error);
+    }
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  response => {
+    if (response.status === 403) {
+      alert("please use your API key to run the app");
+    }
+    return response;
+  },
+  error => {
+    if (error.response && error.response.data) {
+      return Promise.reject(error.response.data);
+    }
+    return Promise.reject(error.message);
+  }
+);
+
 Vue.config.productionTip = false;
 
 new Vue({
