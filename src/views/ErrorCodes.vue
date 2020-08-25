f328d4fd59802b2de8ecd2c2a3dfa78d9135dc6f
Marta G <marta.guszek@gmail.com>
2020-08-25 16:19:28 +0200

    fixed file with linter

--- src/views/ErrorCodes.vue
+++ src/views/ErrorCodes.vue
@@ -1,13 +1,12 @@
 <template>
-    <v-alert type="error" color="pink darken-4">
-      <v-row align="center">
-        <v-col class="grow">
-          <div v-html="errorMessage">
-          </div>
-        </v-col>
-        <v-col class="shrink"> </v-col>
-      </v-row>
-    </v-alert>
+  <v-alert type="error" color="pink darken-4">
+    <v-row align="center">
+      <v-col class="grow">
+        <div v-html="errorMessage"></div>
+      </v-col>
+      <v-col class="shrink"> </v-col>
+    </v-row>
+  </v-alert>
 </template>
 
 <script>
@@ -18,14 +17,12 @@ export default {
       let message = "";
       switch (this.$route.params.id) {
         case 1:
-          message =
-            `<p>
+          message = `<p>
               Your API key is absent. To run the app, make a file named  .env.local. Visit <a target="_blank" rel="noopener noreferrer" href="https://developers.zomato.com/api#headline2" class="text-decoration-none font-weight-black pink--text text--lighten-3">Zomato</a> and request your API key. Then write in .env.local: VUE_APP_API_KEY=your API KEY. Save it. Run in the console <code>npm run serve</code>. For more information, read <a target="_blank" rel="noopener noreferrer" href="https://github.com/MartaAG/bestRestaurants/blob/fixBugs/README.md" class="text-decoration-none font-weight-black pink--text text--lighten-3">Readme</a> file.
             </p>`;
           break;
         case 403:
-          message =
-            `<p>
+          message = `<p>
               Your key expired or is invalid. Visit <a target="_blank" rel="noopener noreferrer" class="text-decoration-none font-weight-black pink--text text--lighten-3" href="https://developers.zomato.com/api#headline2">Zomato</a> and request a new API key. For more information, read <a target="_blank" rel="noopener noreferrer" href="https://github.com/MartaAG/bestRestaurants/blob/fixBugs/README.md" class="text-decoration-none font-weight-black pink--text text--lighten-3">Readme</a> file.
             </p>`;
           break;
