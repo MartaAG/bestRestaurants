db90d6eee8a10f68dc96cdbf2458448cb42bd181
Marta G <marta.guszek@gmail.com>
2020-09-07 15:21:37 +0200

    run linter

--- src/components/RestaurantDetails.vue
+++ src/components/RestaurantDetails.vue
@@ -2,65 +2,64 @@
   <div v-if="restaurant">
     <v-row>
       <v-col lg="4">
-
-    <v-card  class="mx-auto"
-    max-width="500"
-    elevation="15">
-      <v-card-text>
-      <h2 class="display-1 text--primary">{{ restaurant.name }}</h2>
-      <h3 class="text--primary">{{ restaurant.location.address }}</h3>
-      <p>Hours: {{ restaurant.timings }}</p>
-      <p>Phone: {{ restaurant.phone_numbers }}</p>
-      </v-card-text>
-    </v-card>
-    <br />
-    <p>
-      <span class="font-weight-medium">Cuisines:</span>
-      {{ restaurant.cuisines }}
-    </p>
-    <p>
-      <span class="font-weight-medium">Average cost for two customers:</span>
-      {{ restaurant.average_cost_for_two }}
-      {{ restaurant.currency }}
-    </p>
-    <p>
-      <span class="font-weight-medium">Rating: </span>
-      <span
-        >{{ restaurant.user_rating.aggregate_rating }},
-        {{ restaurant.user_rating.rating_text }}</span
-      >
-      <span class="text--secondary">
-        ({{ restaurant.all_reviews_count }} ratings)</span
-      >
-    </p>
-    <p v-if="restaurant.has_online_delivery === 0">
-      No online delivery
-    </p>
-    <p v-else>
-      There is online delivery
-    </p>
-    <p v-if="restaurant.has_table_booking === 0">
-      No table booking
-    </p>
-    <p v-else>
-      There is table booking
-    </p>
-    <div>
-      <h4>Links</h4>
-      <ul>
-        <li>
-          <a :href="restaurant.url">{{ restaurant.name }}</a
-          ><br />
-          <a :href="restaurant.photos_url">Photos</a><br />
-          <a :href="restaurant.menu_url">Menu</a><br />
-          <a :href="restaurant.events_url">Events</a><br />
-        </li>
-      </ul>
-    </div>
-    <br />
-    <v-btn to="/" color="brown darken-1" dark>
-      go back
-    </v-btn>
+        <v-card class="mx-auto" max-width="500" elevation="15">
+          <v-card-text>
+            <h2 class="display-1 text--primary">{{ restaurant.name }}</h2>
+            <h3 class="text--primary">{{ restaurant.location.address }}</h3>
+            <p>Hours: {{ restaurant.timings }}</p>
+            <p>Phone: {{ restaurant.phone_numbers }}</p>
+          </v-card-text>
+        </v-card>
+        <br />
+        <p>
+          <span class="font-weight-medium">Cuisines:</span>
+          {{ restaurant.cuisines }}
+        </p>
+        <p>
+          <span class="font-weight-medium"
+            >Average cost for two customers:</span
+          >
+          {{ restaurant.average_cost_for_two }}
+          {{ restaurant.currency }}
+        </p>
+        <p>
+          <span class="font-weight-medium">Rating: </span>
+          <span
+            >{{ restaurant.user_rating.aggregate_rating }},
+            {{ restaurant.user_rating.rating_text }}</span
+          >
+          <span class="text--secondary">
+            ({{ restaurant.all_reviews_count }} ratings)</span
+          >
+        </p>
+        <p v-if="restaurant.has_online_delivery === 0">
+          No online delivery
+        </p>
+        <p v-else>
+          There is online delivery
+        </p>
+        <p v-if="restaurant.has_table_booking === 0">
+          No table booking
+        </p>
+        <p v-else>
+          There is table booking
+        </p>
+        <div>
+          <h4>Links</h4>
+          <ul>
+            <li>
+              <a :href="restaurant.url">{{ restaurant.name }}</a
+              ><br />
+              <a :href="restaurant.photos_url">Photos</a><br />
+              <a :href="restaurant.menu_url">Menu</a><br />
+              <a :href="restaurant.events_url">Events</a><br />
+            </li>
+          </ul>
+        </div>
+        <br />
+        <v-btn to="/" color="brown darken-1" dark>
+          go back
+        </v-btn>
       </v-col>
     </v-row>
   </div>
