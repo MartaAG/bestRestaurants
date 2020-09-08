ca75266c96b29fc589f722cbf1047c8037f49095
Marta G <marta.guszek@gmail.com>
2020-09-08 18:56:45 +0200

    Unit tests don't work

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,33 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import Vuetify from 'vuetify'
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
+import App from "@/App.vue";
+
+import { mount, createLocalVue } from "@vue/test-utils";
+
+
+
+
+describe("App.vue", () => {
+  let vuetify;
+  let wrapper;
+
+  beforeEach(() => {
+    const localVue = createLocalVue()
+    vuetify = new Vuetify()
+    localVue.use(vuetify)
+
+    wrapper = mount(App, {
+      localVue
+    })
+  })
+
+  it('should match snapshot', () => {
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+  it('should render a vue instance', () => {
+    expect(mount(App).isVueInstance()).toBe(true);
+  })
+  it('should have footer'), () => {
+      expect(wrapper.contains("footer")).toBe(true);
+  }
 });
