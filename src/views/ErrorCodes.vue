0fc817a353db6a36f613748ae446cdd97ce775fe
Marta G <marta.guszek@gmail.com>
2020-09-09 14:25:19 +0200

    refactor switch code

--- src/views/ErrorCodes.vue
+++ src/views/ErrorCodes.vue
@@ -16,27 +16,21 @@ export default {
   name: "ErrorCodes",
   computed: {
     errorMessage: function() {
-      let message = "";
+      //let message = "";
       switch (this.$route.params.id) {
         case "noKey":
-          message = `<p>
+          return `<p>
               Your API key is absent. To run the app, make a file named  .env.local. Visit <a target="_blank" rel="noopener noreferrer" href="https://developers.zomato.com/api#headline2" class="text-decoration-none font-weight-black pink--text text--lighten-3">Zomato</a> and request your API key. Then write in .env.local: VUE_APP_API_KEY=your API KEY. Save it. Run in the console <code>npm run serve</code>. For more information, read <a target="_blank" rel="noopener noreferrer" href="https://github.com/MartaAG/bestRestaurants/blob/fixBugs/README.md" class="text-decoration-none font-weight-black pink--text text--lighten-3">Readme</a> file.
             </p>`;
-          break;
         case "403":
-          message = `<p>
+          return `<p>
               Your key expired or is invalid. Visit <a target="_blank" rel="noopener noreferrer" class="text-decoration-none font-weight-black pink--text text--lighten-3" href="https://developers.zomato.com/api#headline2">Zomato</a> and request a new API key. For more information, read <a target="_blank" rel="noopener noreferrer" href="https://github.com/MartaAG/bestRestaurants/blob/fixBugs/README.md" class="text-decoration-none font-weight-black pink--text text--lighten-3">Readme</a> file.
             </p>`;
-          break;
         case "404":
-          message = `<p>
-          Page you requested does not exist.
-          </p>`;
-          break;
+          return `<p> Page you requested does not exist. </p>`;
         default:
-          message = `Unknown error.`;
+          return `Unknown error.`;
       }
-      return message;
     }
   }
 };
